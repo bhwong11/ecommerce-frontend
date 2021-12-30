e98193cf094a24eb3c08b32e1eb05c352f0301f9
bhwong11 <user@email.com>
2021-12-29 21:06:02 -0500

    apollo client quering correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
+import { ApolloConsumer } from '@apollo/client';
 import './App.css';
-import Test from "./Test";
 
 import {
     ApolloClient,
@@ -12,34 +12,33 @@ import {
     useMutation,
   } from "@apollo/client";
 
-const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
-    cache: new InMemoryCache()
-  });
 
 const GET_REVIEWS = gql`
   query{
-      reviews{
-          _id
-          user{
-            username
-          }
-          product{
-            name
-            price
-          }
-          content
+    reviews{
+      title,
+      _id,
+      product{
+        title
+      },
+      user{
+        username
       }
+    }
   }`;
 
 function App() {
   const [reviews,setReviews] = useState<any>(null)
   const {loading,data} = useQuery<any>(GET_REVIEWS)
-  //useEffect(()=>{
-  //  setReviews(queryReviews)
-  //})
+  console.log(data)
+  
   return (
-    <Test/>
+    <>
+    {data?<div>{data.reviews[0].title}</div>:<div>loading</div>}
+    <div>
+      hello world
+    </div>
+    </>
   );
 }
 
