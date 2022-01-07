7aa998b206ffe0780749efb9791536abbac9dadb
bhwong11 <user@email.com>
2022-01-07 13:48:26 -0500

    added cat query

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,6 +1,31 @@
 import React,(useEffect,useState) from 'react';
+import { useParams,Link,useNavigate } from "react-router-dom";
+const { categoryId } = useParams();
+import {
+    useQuery,
+    gql,
+    useMutation,
+  } from "@apollo/client";
+
+const GET_CATEGORY = gql`
+  query{
+    categories{
+      name,
+      _id,
+      products{
+        _id,
+        title,
+        price,
+        image,
+        description,
+        user,
+        category,
+      }
+    }
+  }`;
 
 const Category:React.FC=()=>{
+    
     return(
         <>
             Category Page
